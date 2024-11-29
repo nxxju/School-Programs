1f704a14ea451fed250f4ef37a81b0077565a3bd
Ninju <128250644+nxxju@users.noreply.github.com>
2024-11-29 23:23:20 +0530

    Delete Program 9.py

--- Program 9.py
+++
@@ -1,8 +0,0 @@
-x = [10, 20, 30, 40, 50]
-
-y = int(input("Enter an element to check: "))
-
-if y in x:
-    print("The element is present in the list.")
-else:
-    print("The element is not present in the list.")
