905c6fe91dc720f25587de6860c52bd86e5a5d52
Ninju <128250644+nxxju@users.noreply.github.com>
2024-11-29 23:02:02 +0530

    Update Program 1.py

--- Program 1.py
+++ Program 1.py
@@ -1,5 +1,5 @@
-name = input("Enter your name: ")
-class = input("Enter your class: ")
+x = input("Enter your name: ")
+y = input("Enter your class: ")
 
-print("Name:", name)
-print("Class:", class)
+print("Name:", x)
+print("Class:", y)
