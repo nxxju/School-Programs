b87f9bb64f64dda4286df4c3157bf9444465ea6a
Ninju <128250644+nxxju@users.noreply.github.com>
2024-11-29 23:22:25 +0530

    Delete Program 15.py

--- Program 15.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread('image.jpg')  # Replace with your image file path
-
-if img is None:
-    print("Error: Image not found.")
-else:
-    pixel_value = img[100, 100]
-    print("Red:", pixel_value[2], "Green:", pixel_value[1], "Blue:", pixel_value[0])
