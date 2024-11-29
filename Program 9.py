0eeb4273ae691396627d07bf928e1c65354a35e5
Ninju <128250644+nxxju@users.noreply.github.com>
2024-11-29 22:21:33 +0530

    Create Program 9.py

---
+++ Program 9.py
@@ -0,0 +1,8 @@
+x = [10, 20, 30, 40, 50]
+
+y = int(input("Enter an element to check: "))
+
+if y in x:
+    print("The element is present in the list.")
+else:
+    print("The element is not present in the list.")
