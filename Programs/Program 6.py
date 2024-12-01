ab8599604e44c3536941d6515c32b524b9bbb5f1
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-01 13:49:01 +0530

    Update Program 6.py

--- Programs/Program 6.py
+++ Programs/Program 6.py
@@ -1,11 +1,10 @@
 x = int(input("Enter a number: "))
-y = 1
-
-if x < 0:
+if x<0:
     print("Factorial is not defined for negative numbers.")
-elif x == 0:
-    print("The factorial of 0 is 1.")
+elif x==0:
+    print("The factorial of", x, "is 1")
 else:
-    for i in range(1, x + 1):
-        y*= i
-    print(f"The factorial of {x} is {y}.")
+    y = 1
+    for i in range(1, x+1):
+        y = y*i
+    print("The factorial of", x, "is", y)
