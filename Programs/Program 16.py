3267966c3c07213f2ee95c057b87263658acf9b8
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-18 13:03:29 +0530

    Create Program 16.py

---
+++ Programs/Program 16.py
@@ -0,0 +1,9 @@
+l = float(input("Enter length: "))
+w = float(input("Enter width: "))
+h = float(input("Enter height: "))
+
+sa = 2 * (l * w + l * h + w * h)
+v = l * w * h
+
+print("Surface area:- ", sa)
+print("Volume:- ", v)
