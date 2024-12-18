a66113bbc152758e46d7393ac0f1506370de310c
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-18 12:47:38 +0530

    Update Program 13.py

--- Programs/Program 13.py
+++ Programs/Program 13.py
@@ -1,3 +1,5 @@
+# Python program to create a bar chart using the Matplotlib library to compare student’s marks: Stud_name= [“Priya”,” Angel”,” Diaz”,” Mike”,” Ryan”] Stud_marks= [45,70,89,50,92]
+
 import matplotlib.pyplot as plt
 
 x = ["Surya", "Sumedh", "Aaron", "Rayaansh", "Ishaan"]
