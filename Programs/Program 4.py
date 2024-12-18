a2a3377b8cb19ba5f6a016daf449d892ffd6a906
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-18 12:42:16 +0530

    Update Program 4.py

--- Programs/Program 4.py
+++ Programs/Program 4.py
@@ -1,3 +1,5 @@
+# Program to find the largest number among the three input numbers.
+
 x = float(input("Enter the first number: "))
 y = float(input("Enter the second number: "))
 z = float(input("Enter the third number: "))
