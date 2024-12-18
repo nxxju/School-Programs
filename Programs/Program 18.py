1d3a2d872761e0da6e7dc37880c45643e1590cf0
Ninju <128250644+nxxju@users.noreply.github.com>
2024-12-18 15:37:05 +0530

    Create Program 18.py

---
+++ Programs/Program 18.py
@@ -0,0 +1,12 @@
+x=int(input("Please enter a number: "))
+
+if x > 0:
+    if x % 2 ==0:
+        print("The number is positive and even")
+    else:
+        print("The number is positive and odd")
+else:
+    if x % 2 ==0:
+        print("The number is negative and even")
+    else:
+        print("The number is negative and odd.") 
